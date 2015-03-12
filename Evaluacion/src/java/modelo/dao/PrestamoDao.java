e907649091f7e51704e039c5232564745748ad11
ladyaguilar <lmaguilar@misena.edu.co>
2015-03-11 23:32:45 -0500

    Validación del usuario

--- Evaluacion/src/java/modelo/dao/PrestamoDao.java
+++ Evaluacion/src/java/modelo/dao/PrestamoDao.java
@@ -5,10 +5,37 @@
  */
 package modelo.dao;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import utilidades.Conexion;
+
 /**
  *
  * @author Sena
  */
 public class PrestamoDao {
+    PreparedStatement ps = null;
+        Connection cn = null;
+        ResultSet rs = null;
+        
+        public PrestamoDao() {
+        cn = Conexion.getInstance();
+    }
+        public int validarUsuario(int idUsuario){
+            String mensaje="";
+            CallableStatement cstm;
+            
+            try{
+                cstm = cn.prepareCall("{call sp_validarUsuario(?) }");
+                cstm.setInt(1, idUsuario);
+                cstm.executeQuery();
+            } catch (SQLException ex) {
+            mensaje = "Error: " + ex.getMessage();
+        }
+            return 0;
+        }
     
 }
--- Evaluacion/src/java/modelo/dao/UsuarioDao.java
+++ Evaluacion/src/java/modelo/dao/UsuarioDao.java
@@ -5,10 +5,26 @@
  */
 package modelo.dao;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import utilidades.Conexion;
+
+
 /**
  *
  * @author Sena
  */
 public class UsuarioDao {
+        PreparedStatement ps = null;
+        Connection cn = null;
+        ResultSet rs = null;
+        
+        public UsuarioDao() {
+        cn = Conexion.getInstance();
+    }
+        
     
 }
